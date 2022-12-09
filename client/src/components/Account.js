b55be7f149ad50131e494c50516b415d3fb963b4
rodgersochieng <sirrodgers21@gmail.com>
2022-12-09 09:04:31 +0300

    complete backend

--- client/src/components/Account.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Account() {
-  return <>Homepage</>;
-}
-
-export default Account;
--- client/src/components/Homepage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Homepage() {
-  return <>Homepage</>;
-}
-
-export default Homepage;
